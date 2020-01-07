3bb61791222f22bc0ce093fb421c98d6d065090e
Pol Timmer <pol.timmer@gmail.com>
2020-01-07 12:03:42 +0100

    Added constants for network size

--- MLP.py
+++ MLP.py
@@ -20,6 +20,7 @@ def reLu(M):
 
     return W
 
+
 def softmax(raw_preds):
     """
     pass raw predictions through softmax activation function
@@ -28,19 +29,24 @@ def softmax(raw_preds):
     # divide exponentiated vector by its sum. All values in the output sum to 1
     return out / np.sum(out)
 
+
 x = df[["X_0", "X_1"]].to_numpy().T
 
 n = x.shape[1]
 
-W0 = np.ones((2, 10))
-W1 = np.ones((10, 10))
-W2 = np.ones((10, 2))
+HID_0_SIZE = 10
+HID_1_SIZE = 10
+OUT_SIZE = 2
 
-b0 = np.zeros((10, 1))
-b1 = np.zeros((10, 1))
-b2 = np.zeros((1, 1))
+W0 = np.ones((2, HID_0_SIZE))
+W1 = np.ones((HID_0_SIZE, HID_1_SIZE))
+W2 = np.ones((HID_1_SIZE, OUT_SIZE))
 
-#function within reLu might be wrong
+b0 = np.zeros((HID_0_SIZE, 1))
+b1 = np.zeros((HID_1_SIZE, 1))
+b2 = np.zeros((OUT_SIZE, 1))
+
+# function within reLu might be wrong
 h0 = reLu(W0.T @ x + np.repeat(b0, n, axis=1))
 h1 = reLu(W1.T @ h0 + np.repeat(b1, n, axis=1))
 output = softmax(W2.T @ h1 + np.repeat(b2, n, axis=1))
