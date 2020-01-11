0345b078a829cd56bbd9b57c1993f86796956614
Pol Timmer <pol.timmer@gmail.com>
2020-01-11 19:31:30 +0100

    Fixed the network! mainly by initing parameters randomly so they could diverge. Also fixed some methods that modified inputs.

--- MLP.py
+++ MLP.py
@@ -7,9 +7,10 @@ from matplotlib import pyplot as plt
 
 IN_SIZE = 2
 HID_0_SIZE = 10
-HID_1_SIZE = 5
-OUT_SIZE = 2
-learning_rate = 0.01
+HID_1_SIZE = 10
+OUT_SIZE = 1
+learning_rate = 0.03
+RELU_LEAK = 0.01
 
 df = pd.read_csv('./HW3train.csv')
 
@@ -20,39 +21,30 @@ plt.ylabel('X_1')
 plt.show()
 
 
-
-def ReLU(v):
+def ReLU(v): # is nu een leaky ReLU omdat ik dacht dat we te maken hadden met vanishing gradient, maar kan weg denk ik
     w = np.zeros(v.shape)
     for i, val in enumerate(v):
-        w[i] = max(0, val)
+        w[i] = max(RELU_LEAK * val, val)
 
     return w
 
-    # def softmax(raw_preds):
-    """
-    pass raw predictions through softmax activation function
-    """
+def ReLUDerivative(v):
+    res = np.copy(v)
+    for n in np.nditer(res, op_flags=['readwrite']):
+        n[...] = 1 if n >= 0 else RELU_LEAK
 
-
-#    out = np.exp(raw_preds)  # exponentiate vector of raw predictions
-# divide exponentiated vector by its sum. All values in the output sum to 1
-#    return out / np.sum(out)
-
-def ReLUDerivative(x):
-    for n in np.nditer(x, op_flags=['readwrite']):
-        n[...] = 1 if n >= 0 else 0
-
-    return x
+    return res
 
 
 def sigmoid(raw_preds):
+    res = np.zeros(raw_preds.shape)
     for i, gamma in enumerate(raw_preds):
         if gamma < 0:
-            raw_preds[i] = 1 - 1 / (1 + math.exp(gamma))
+            res[i] = 1 - 1 / (1 + math.exp(gamma))
         else:
-            raw_preds[i] = 1 / (1 + math.exp(-gamma))
+            res[i] = 1 / (1 + math.exp(-gamma))
 
-    return raw_preds
+    return res
 
 
 def sigmoidDerivative(x):
@@ -60,7 +52,8 @@ def sigmoidDerivative(x):
 
 
 def grad_o(o, y):
-    return o - np.array([y, 1 - y])
+    # return o - np.array([y, 1 - y])
+    return o - y
 
 
 def grad_q(q, o, y):
@@ -82,17 +75,25 @@ seed(1)
 # W1 = np.full((HID_0_SIZE, HID_1_SIZE), 1 / HID_0_SIZE)
 # W2 = np.full((HID_1_SIZE, OUT_SIZE), 1 / HID_1_SIZE)
 
-W0 = np.ones((IN_SIZE, HID_0_SIZE))
-W1 = np.ones((HID_0_SIZE, HID_1_SIZE))
-W2 = np.ones((HID_1_SIZE, OUT_SIZE))
+W0 = np.random.rand(IN_SIZE, HID_0_SIZE) - 0.5
+W1 = np.random.rand(HID_0_SIZE, HID_1_SIZE) - 0.5
+W2 = np.random.rand(HID_1_SIZE, OUT_SIZE) - 0.5
 
-b0 = np.zeros(HID_0_SIZE)
-b1 = np.zeros(HID_1_SIZE)
-b2 = np.zeros(OUT_SIZE)
+# W0 = np.ones((IN_SIZE, HID_0_SIZE))
+# W1 = np.ones((HID_0_SIZE, HID_1_SIZE))
+# W2 = np.ones((HID_1_SIZE, OUT_SIZE))
 
-L = 1
+b0 = np.random.rand(HID_0_SIZE) - 0.5
+b1 = np.random.rand(HID_1_SIZE) - 0.5
+b2 = np.random.rand(OUT_SIZE) - 0.5
 
-while L >= 0.1:
+# b0 = np.zeros(HID_0_SIZE)
+# b1 = np.zeros(HID_1_SIZE)
+# b2 = np.zeros(OUT_SIZE)
+
+L = 1 # wordt nog niet gebruikt
+
+for i in range(50000):
     # take random input
     index = randint(0, input.shape[1] - 1)
     x = np.array(input[:, index]).T
@@ -129,6 +130,24 @@ while L >= 0.1:
 
     W2 -= learning_rate * grad_W2
     b2 -= learning_rate * grad_b2
+
+
+predicts = np.zeros(input.shape[1])
+for i in range(input.shape[1]):
+    x = np.array(input[:, i]).T
+    y = df.loc[i, "y"]
+
+    # forward pass
+    r = (W0.T @ x) + b0
+    h0 = ReLU(r)
+    p = (W1.T @ h0) + b1
+    h1 = ReLU(p)
+    q = (W2.T @ h1) + b2
+    output = sigmoid(q)
+    predicts[i] = output
+
+# NOW PLOT PREDICTS
+
 
 # derivatives voor alle variabelen.
 # grad_w0 = x (outer prod) grad_r // geeft een matrix
